b499afa74b721e50e1630a58af0c37cf281bbb29
Herminio Torres <herminiocesar@gmail.com>
2011-04-04 13:03:50 -0300

    Task B: Validation and Unit Testing - Chapter 7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+  # validations
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
+  validates :title, :uniqueness   => true,
+                    :length       => { 
+                      :minimum => 10, 
+                      :message => 'must be at least ten characters long.'
+                    }
+  validates :image_url, :format   => { 
+      :with    => %r{\.(gif|jpg|png)$}i,
+      :message => 'must be a URL for GIF, JPG or PNG image.'
+    }
 end
